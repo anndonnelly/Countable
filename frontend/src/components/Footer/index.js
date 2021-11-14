8c5651183022b502f47ac5f83a6e6f049b94c0c8
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-14 13:14:47 -0500

    first commit

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import "./Footer.css";
+function Footer (){
+  return (
+    <div className="footerDiv">
+      <div className="socialMedia">
+        <label> Created by: Ann Donnelly</label>
+        <a href="https://github.com/anndonnelly">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            alt="githubLogo"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/ann-donnelly/">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+            alt="linkedInLogo"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
