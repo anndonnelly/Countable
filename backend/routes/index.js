654a811ee75414ac2090316a3b6b4134f477fc5b
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-14 20:58:51 -0500

    csrf issue fixed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,13 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    const token = req.csrfToken();
+    res.cookie("XSRF-TOKEN", token);
+    res.sendStatus(200);
+  });
+}
 
 
 module.exports = router;
