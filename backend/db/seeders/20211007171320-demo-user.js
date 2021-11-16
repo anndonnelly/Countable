4f436121f79dd6cb73965ce89e1e865aa39a341a
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-16 10:08:06 -0500

    users seeder data

--- backend/db/seeders/20211007171320-demo-user.js
+++ backend/db/seeders/20211007171320-demo-user.js
@@ -17,15 +17,140 @@ module.exports = {
           username: "DemoTwo",
           hashedPassword: bcrypt.hashSync("password"),
         },
+        // {
+        //   email: faker.internet.email(),
+        //   username: "FakeUser1",
+        //   hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        // },
+        // {
+        //   email: faker.internet.email(),
+        //   username: "FakeUser2",
+        //   hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        // },
         {
-          email: faker.internet.email(),
-          username: "FakeUser1",
-          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          email: "donnelly.ann21@gmail.com",
+          username: "anndonnelly",
+          hashedPassword: bcrypt.hashSync("password"),
         },
         {
-          email: faker.internet.email(),
-          username: "FakeUser2",
-          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          email: "patriciambdonnelly@gmail.com",
+          username: "pdonnelly",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "nmadonnelly@gmail.com",
+          username: "noelmario",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "bdonnelly090@gmail.com",
+          username: "bdonnz",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "amymarieward@hotmail.com",
+          username: "amymarie",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "emily.obrien53@mail.dcu.ie",
+          username: "emilyobrien",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "roblynch@gmail.com",
+          username: "roblynch",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "anniemonaghan3@gmail.com",
+          username: "anniemonaghan",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "Rachel.Fitzpatrick@ergogroup.ie",
+          username: "rachfitz",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "jenfitz@live.ie",
+          username: "jenfitz",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "jill.donnelly27@mail.dcu.ie",
+          username: "jilldonnelly",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "juliewagner@gmail.com",
+          username: "juliewagner",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "curtiscassell@gmail.com",
+          username: "curtiscassell",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "noahspencer@gmail.com",
+          username: "noahspencer",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "parkershaw@gmail.com",
+          username: "parkershaw",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "charlieward18@gmail.com",
+          username: "charlieward",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "paigelahaie@gmail.com",
+          username: "paigelahaie",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "elizabethspencer@gmail.com",
+          username: "lizspencer",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "danpurcell@gmail.com",
+          username: "danpurcell",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "allyfoley@gmail.com",
+          username: "allyfoley",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "lucymccourt@gmail.com",
+          username: "lucymccourt",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "michaelglynn@gmail.com",
+          username: "michaelglynn",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "megancaseydiviney@gmail.com",
+          username: "megancaseydiviney",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "patrickstory@gmail.com",
+          username: "patrickstory",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "maevedonnelly@gmail.com",
+          username: "maevedonnelly",
+          hashedPassword: bcrypt.hashSync("password"),
         },
       ],
       {}
@@ -37,9 +162,40 @@ module.exports = {
     return queryInterface.bulkDelete(
       "Users",
       {
-        username: { [Op.in]: ["Demo-lition", "DemoTwo", "FakeUser1", "FakeUser2"] },
+        username: {
+          [Op.in]: [
+            "Demo-lition",
+            "DemoTwo",
+            "anndonnelly",
+            "pdonnelly",
+            "noelmario",
+            "bdonnz",
+            "amymarie",
+            "emilyobrien",
+            "roblynch",
+            "anniemonaghan",
+            "rachfitz",
+            "jenfitz",
+            "jilldonnelly",
+            "juliewagner",
+            "curtiscassell",
+            "noahspencer",
+            "parkershaw",
+            "charlieward",
+            "paigelahaie",
+            "lizspencer",
+            "danpurcell",
+            "allyfoley",
+            "lucymccourt",
+            "michaelglynn",
+            "megancaseydiviney",
+            "patrickstory",
+            "maevedonnelly",
+          ],
+        },
       },
       {}
     );
   },
 };
+
