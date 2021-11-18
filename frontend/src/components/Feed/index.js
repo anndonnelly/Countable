a40206a48f042782a4e4d1e840e5116b7ef1ed75
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-18 10:45:35 -0500

    nav bar started

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -3,6 +3,7 @@ import { getAllPostsThunk } from "../../store/posts";
 import { useDispatch, useSelector } from "react-redux";
 import PostCard from "../CountablePosts/PostCard";
 import CreatePostModal from "../CountablePosts/CreatePostModal";
+import "./Feed.css"
 
 
 function Feed() {
@@ -15,16 +16,12 @@ function Feed() {
     dispatch(getAllPostsThunk());
   }, [dispatch]);
 
-//   useEffect(() => {
-//     setCurrentPosts(currentPosts);
-//   }, [currentPosts]);
+
 
   return (
     <>
-      <div>
-        <CreatePostModal />
-      </div>
-      <div>
+      
+      <div className="feedContainer">
         {posts.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,24 +3,42 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import CreatePostModal from "../CountablePosts/CreatePostModal";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
+  let navBar;
   if (sessionUser) {
-    sessionLinks = <ProfileButton user={sessionUser} />;
-  } else {
-    // sessionLinks = (
-    //   <>
-    //     <NavLink to="/">Log In</NavLink>
-    //     <NavLink to="/signup">Sign Up</NavLink>
-    //   </>
-    // );
+    navBar = (
+      <div className="navWrapper">
+        <div className="nav">
+          <div className="logoWrapper">
+            <img
+              className="logo"
+              src={
+                "https://res.cloudinary.com/dis83syog/image/upload/v1636988652/Countable/Screen_Shot_2021-11-15_at_9.44.55_AM_eoweib.png"
+              }
+              alt=""
+            ></img>
+          </div>
+          <div className="searchBar">
+            <input></input>
+          </div>
+          <div className="actionButtons">
+            <div>
+              <CreatePostModal />
+            </div>
+            <NavLink to="/">Home</NavLink>
+            <ProfileButton user={sessionUser} />;
+          </div>
+        </div>
+      </div>
+    );} else {
     return null
   }
 
-  return <div>{isLoaded && sessionLinks}</div>;
+  return <div>{isLoaded && navBar}</div>;
 }
 
 export default Navigation;
