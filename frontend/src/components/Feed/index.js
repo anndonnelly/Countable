59a16324eff6498597a595bb3a2e0b137a072dbf
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-28 13:42:01 -0500

    final styling

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -8,11 +8,10 @@ function Feed() {
   const dispatch = useDispatch();
 
   const posts = useSelector((state) => Object.values(state.posts));
- 
+
   useEffect(() => {
     dispatch(getAllPostsThunk());
   }, [dispatch]);
-
 
   return (
     <>
